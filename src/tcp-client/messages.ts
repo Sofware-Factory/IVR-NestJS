ef0c3e5b847a20f56121ceb5e598e1328a2d4fd0
Miguel Angel Ruiz Patarroyo <maruizpa@Miguels-MacBook-Air.local>
2023-11-08 15:11:38 -0500

    expertos ip - Changes

--- src/tcp-client/messages.ts
+++ src/tcp-client/messages.ts
@@ -1,6 +1,6 @@
 // messages.ts
 export const firstMessage =
-  'MessageID=1\nPassword=2yQQC[<6KiXuMM`];%R&lv>-1,)a/Cfheacbc~|wI+DE:19[w4$TAqb`9GX(~r^.\nMessageType=BopJob\nPhoneNumber=3204525560\nDialledNumber=6600000\nAddResponseID=true\nBOP=false\nCLI=true'; // Tu primer mensaje aquí
+  'MessageID=1\nPassword=2yQQC[<6KiXuMM`];%R&lv>-1,)a/Cfheacbc~|wI+DE:19[w4$TAqb`9GX(~r^.\nMessageType=BopJob\nPhoneNumber=3136722950\nDialledNumber=6600000\nAddResponseID=true\nBOP=false\nCLI=true'; // Tu primer mensaje aquí
 
 export const secondMessage =
   'MessageID=1\nMessageType=BookingRequest\nPassword=2yQQC[<6KiXuMM`];%R&lv>-1,)a/Cfheacbc~|wI+DE:19[w4$TAqb`9GX(~r^.\nFromCliBooking=${result.newResponseId},1\nBookedForNow=true\nPhoneSystemID=7778\nDialledNumber=6600000\nBaseAddress=1\n\n'; // Tu segundo mensaje aquí
--- src/tcp-client/tcp-client.controller.ts
+++ src/tcp-client/tcp-client.controller.ts
@@ -21,7 +21,7 @@ export class TcpClientController {
     const responseId = this.tcpClientService.getResponseId();
 
     // Construye el mensaje directamente aquí en lugar de usar secondMessage
-    const newMessage = `MessageID=1\nMessageType=BookingRequest\nPassword=2yQQC[<6KiXuMM\`];%R&lv>-1,)a/Cfheacbc~|wI+DE:19[w4$TAqb\`9GX(~r^.\nFromCliBooking=4052,1\nBookedForNow=true\nPhoneSystemID=7778\nDialledNumber=6600000\nBaseAddress=1`;
+    const newMessage = `MessageID=1\nMessageType=BookingRequest\nPassword=2yQQC[<6KiXuMM\`];%R&lv>-1,)a/Cfheacbc~|wI+DE:19[w4$TAqb\`9GX(~r^.\nFromCliBooking=1469,2\nBookedForNow=true\nPhoneSystemID=7778\nDialledNumber=6600000\nBaseAddress=1`;
 
     const response = await this.tcpClientService.sendMessage(newMessage, true);
 
